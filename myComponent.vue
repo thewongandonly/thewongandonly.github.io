13e107b9472932b68a044c023c621b6679a4d6d8
David Wong <1740957+thewongandonly@users.noreply.github.com>
2024-04-06 13:42:41 +0200

    Add files via upload

---
+++ myComponent.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="full-height">
+    <div class="container">
+      <div class="centered-element">
+        <div class="content">
+          <div class="inner">
+            <img
+              src="https://thewongandonly.com/Wongcircle.png"
+              height="200"
+              alt="David Wong"
+            />
+
+            <h1>David Wong</h1>
+            <h1>thewongandonly.com</h1>
+
+            <p style="font-size: 0.9rem">
+              fullstack C# developer<br />
+              I use vue.js &amp; C# to create dockerized microservices for
+              remarkable digital experiences. <br />
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "world!",
+      color: "blue",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: grid;
+  align-items: center;
+  height: 600px;
+  border: 0px solid #006100;
+}
+
+.full-height {
+  height: 100vh;
+  width: 100%;
+  color: #f3e1d1;
+  background: #212629;
+  font-family: "Montserrat", sans-serif !important;
+
+  display: -moz-flex;
+  display: -webkit-flex;
+  display: -ms-flex;
+  display: flex;
+  -moz-flex-direction: column;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -moz-align-items: center;
+  -webkit-align-items: center;
+  -ms-align-items: center;
+  align-items: center;
+  -moz-transition: -moz-transform 0.325s ease-in-out,
+    -moz-filter 0.325s ease-in-out, opacity 0.325s ease-in-out;
+  -webkit-transition: -webkit-transform 0.325s ease-in-out,
+    -webkit-filter 0.325s ease-in-out, opacity 0.325s ease-in-out;
+  -ms-transition: -ms-transform 0.325s ease-in-out,
+    -ms-filter 0.325s ease-in-out, opacity 0.325s ease-in-out;
+  transition: transform 0.325s ease-in-out, filter 0.325s ease-in-out,
+    opacity 0.325s ease-in-out;
+  background-image: -moz-radial-gradient(
+    rgba(0, 0, 0, 0.25) 25%,
+    rgba(0, 0, 0, 0) 55%
+  );
+  background-image: -webkit-radial-gradient(
+    rgba(0, 0, 0, 0.25) 25%,
+    rgba(0, 0, 0, 0) 55%
+  );
+  background-image: -ms-radial-gradient(
+    rgba(0, 0, 0, 0.25) 25%,
+    rgba(0, 0, 0, 0) 55%
+  );
+  background-image: radial-gradient(
+    rgba(0, 0, 0, 0.25) 25%,
+    rgba(0, 0, 0, 0) 55%
+  );
+  max-width: 100%;
+  text-align: center;
+}
+</style>
