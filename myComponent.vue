694c8641bc49464d5d6063e3138395ce8f4492d4
David Wong <1740957+thewongandonly@users.noreply.github.com>
2026-08-18 00:24:42 +0200

    Update myComponent.vue

--- myComponent.vue
+++ myComponent.vue
@@ -14,57 +14,104 @@
             <h1>David</h1>
             <h1>thewongandonly</h1>
             <p class="iam">
-
-              <a href="https://x.com/thewongandonly" target="_blank" rel="noopener noreferrer" style="border: none; text-decoration: none" title="contact">
-        <svg width="50px" height="50px" version="1.1" id="svg5" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1668.56 1221.19" style="enable-background: new 0 0 1668.56 1221.19" xml:space="preserve">
-          <style type="text/css">
-            .st0 {
-              stroke: #ffffff;
-              stroke-miterlimit: 10;
-            }
-            .st1 {
-              fill: #ffffff;
-            }
-          </style>
-          <g>
-            <circle class="st0" cx="834.28" cy="610.6" r="481.33"></circle>
-            <g id="layer1" transform="translate(52.390088,-25.058597)">
-              <path id="path1009" class="st1" d="M485.39,356.79l230.07,307.62L483.94,914.52h52.11l202.7-218.98l163.77,218.98h177.32
+              <a
+                href="https://x.com/thewongandonly"
+                target="_blank"
+                rel="noopener noreferrer"
+                style="border: none; text-decoration: none"
+                title="contact"
+              >
+                <svg
+                  width="50px"
+                  height="50px"
+                  version="1.1"
+                  id="svg5"
+                  xmlns:svg="http://www.w3.org/2000/svg"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 1668.56 1221.19"
+                  style="enable-background: new 0 0 1668.56 1221.19"
+                  xml:space="preserve"
+                >
+                  <style type="text/css">
+                    .st0 {
+                      stroke: #ffffff;
+                      stroke-miterlimit: 10;
+                    }
+                    .st1 {
+                      fill: #ffffff;
+                    }
+                  </style>
+                  <g>
+                    <circle
+                      class="st0"
+                      cx="834.28"
+                      cy="610.6"
+                      r="481.33"
+                    ></circle>
+                    <g id="layer1" transform="translate(52.390088,-25.058597)">
+                      <path
+                        id="path1009"
+                        class="st1"
+                        d="M485.39,356.79l230.07,307.62L483.94,914.52h52.11l202.7-218.98l163.77,218.98h177.32
 			L836.82,589.6l215.5-232.81h-52.11L813.54,558.46L662.71,356.79H485.39z M562.02,395.17h81.46l359.72,480.97h-81.46L562.02,395.17
-			z"></path>
-            </g>
-          </g>
-        </svg>
-      </a>
+			z"
+                      ></path>
+                    </g>
+                  </g>
+                </svg>
+              </a>
 
-
-				
-<a href="http://linkedin.com/in/thewongandonlycom" target="_blank" rel="noopener noreferrer" title="linkedin">
-	<svg width="35px" height="35px" viewBox="0 14 46 46" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-		<g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-			<g id="Color-" transform="translate(-702.000000, -265.000000)" fill="#007EBB">
-				<path d="M746,305 L736.2754,305 L736.2754,290.9384 C736.2754,287.257796 734.754233,284.74515 731.409219,284.74515 C728.850659,284.74515 727.427799,286.440738 726.765522,288.074854 C726.517168,288.661395 726.555974,289.478453 726.555974,290.295511 L726.555974,305 L716.921919,305 C716.921919,305 717.046096,280.091247 716.921919,277.827047 L726.555974,277.827047 L726.555974,282.091631 C727.125118,280.226996 730.203669,277.565794 735.116416,277.565794 C741.21143,277.565794 746,281.474355 746,289.890824 L746,305 L746,305 Z M707.17921,274.428187 L707.117121,274.428187 C704.0127,274.428187 702,272.350964 702,269.717936 C702,267.033681 704.072201,265 707.238711,265 C710.402634,265 712.348071,267.028559 712.41016,269.710252 C712.41016,272.34328 710.402634,274.428187 707.17921,274.428187 L707.17921,274.428187 L707.17921,274.428187 Z M703.109831,277.827047 L711.685795,277.827047 L711.685795,305 L703.109831,305 L703.109831,277.827047 L703.109831,277.827047 Z" id="LinkedIn"></path>
-			</g>
-		</g>
-	</svg>
-</a>
+              <a
+                href="http://linkedin.com/in/thewongandonlycom"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="linkedin"
+              >
+                <svg
+                  width="35px"
+                  height="35px"
+                  viewBox="0 14 46 46"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                >
+                  <g
+                    id="Icons"
+                    stroke="none"
+                    stroke-width="1"
+                    fill="none"
+                    fill-rule="evenodd"
+                  >
+                    <g
+                      id="Color-"
+                      transform="translate(-702.000000, -265.000000)"
+                      fill="#007EBB"
+                    >
+                      <path
+                        d="M746,305 L736.2754,305 L736.2754,290.9384 C736.2754,287.257796 734.754233,284.74515 731.409219,284.74515 C728.850659,284.74515 727.427799,286.440738 726.765522,288.074854 C726.517168,288.661395 726.555974,289.478453 726.555974,290.295511 L726.555974,305 L716.921919,305 C716.921919,305 717.046096,280.091247 716.921919,277.827047 L726.555974,277.827047 L726.555974,282.091631 C727.125118,280.226996 730.203669,277.565794 735.116416,277.565794 C741.21143,277.565794 746,281.474355 746,289.890824 L746,305 L746,305 Z M707.17921,274.428187 L707.117121,274.428187 C704.0127,274.428187 702,272.350964 702,269.717936 C702,267.033681 704.072201,265 707.238711,265 C710.402634,265 712.348071,267.028559 712.41016,269.710252 C712.41016,272.34328 710.402634,274.428187 707.17921,274.428187 L707.17921,274.428187 L707.17921,274.428187 Z M703.109831,277.827047 L711.685795,277.827047 L711.685795,305 L703.109831,305 L703.109831,277.827047 L703.109831,277.827047 Z"
+                        id="LinkedIn"
+                      ></path>
+                    </g>
+                  </g>
+                </svg>
+              </a>
 
               <br />
-              fullstack developer since 2000<br />
+              fullstack developer<br />
               {{ msg }} <br />
               Outside of programming, I like
               <span class="clickable" @click="setBackground('YourName.mp4')"
                 >anime</span
-              >,
-              video
-              <span class="clickable" @click="showDoki">games</span>, 
+              >, video <span class="clickable" @click="showDoki">games</span>,
               <span class="clickable" @click="setBackground('Sorry.mp4')"
                 >K-drama</span
               >,
               <span class="clickable" @click="setBackground('bowlramen.mp4')"
                 >Japanese food</span
-              >,
-              and 
+              >, and
               <span class="clickable" @click="onTechno">melodic techno</span>.
             </p>
           </div>
@@ -94,6 +141,7 @@
 
   <div v-if="animateBlue" class="hero-slider curscreen"></div>
 </template>
+
 
 <script>
 export default {
